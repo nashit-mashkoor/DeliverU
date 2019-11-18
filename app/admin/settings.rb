4c56813fdb808b86973818cb4163171bd3cf4c4a
miansaqib05 <l164206@lhr.nu.edu.pk>
2019-11-18 13:15:37 +0500

    Before pulling db models from nashit

--- app/admin/settings.rb
+++ app/admin/settings.rb
@@ -1,13 +1,5 @@
 ActiveAdmin.register_page "Settings" do
   content do
-    #panel "Admin" do
-     # "Register New Admin"
-    #end
     link_to('Admin','/admin/admin_users')
   end
-
-
-  #sidebar "Test Sidebar" do
-    #"Hi World"
-  #end
 end
