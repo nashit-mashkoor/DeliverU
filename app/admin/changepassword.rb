8d042d76b47d94d0b1c3560529b0de1dcae0d2af
Ahsan Naveed <ahsannaveed94@gmail.com>
2019-11-22 18:22:40 +0500

    fixed change password UI

--- app/admin/changepassword.rb
+++ app/admin/changepassword.rb
@@ -2,14 +2,23 @@ ActiveAdmin.register_page "Change Password" do
   menu priority: 2
   content do
     panel 'Please fill all fields' do
-      #form action: "form1", method: :post do |f|
-        #f.inputs 'change password' do
-          #f.input :Cpass, type: :password, label: 'Current Password'
-          #f.input :Npass, type: :password, label: 'New Password'
-          #f.input :RTpass, type: :password, label: 'Retype Password'
-          #f.input :Submit, type: :submit
-        #end
-      #end
+      form action: "form1", method: :post do |f|
+        f.div style: 'display: flex; align-items: center; justify-content: space-between' do
+          f.label "Current Password"
+          f.input :current_password, type: :password, placeholder: 'Current Password'
+        end
+        f.div  style: 'display: flex; align-items: center; justify-content: space-between' do
+          f.label "New Password"
+          f.input :new_password, type: :password, label: 'New Password', placeholder: 'New Password'
+        end
+        f.div style: 'display: flex; align-items: center; justify-content: space-between' do
+          f.label "Confirm Password"
+          f.input :password_confirmation, type: :password, label: 'Confirm Password', placeholder: 'Confirm Password'
+        end
+        f.div do
+          f.input :Submit, type: :submit
+        end
+      end
     end
   end
 end
