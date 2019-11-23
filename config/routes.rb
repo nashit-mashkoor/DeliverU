ccaab4c26ab7850da3bc5dfc8dda049a2090f59e
ignitorkhan <l164104@lhr.nu.edu.pk>
2019-11-22 23:27:43 -0800

    Added Timeslots API

--- config/routes.rb
+++ config/routes.rb
@@ -9,7 +9,12 @@ Rails.application.routes.draw do
   namespace :api do
     namespace :v1 do
       resources :customers
+      resources :timeslots do
+        collection do
+          get 'region_slots/:region_id', to: 'timeslots#region_slots', as: 'region_slotss' 
+        end
+      end
     end
-   end
+  end
 
 end
