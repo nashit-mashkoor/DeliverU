629e6c91b406dd70089dfc850f19ec6671cce75c
miansaqib05 <l164206@lhr.nu.edu.pk>
2019-11-27 22:27:13 +0500

    timeslot graph in analytics

--- app/admin/analytics.rb
+++ app/admin/analytics.rb
@@ -1,6 +1,8 @@
 ActiveAdmin.register_page "Analytics" do
   menu priority: 3
   content do
-    column_chart AdminUser.group(:created_at).count()
+    #column_chart AdminUser.group(:created_at).count()
+    column_chart Order.group(:timeslot_id).count()
+
   end
 end
--- app/admin/regions.rb
+++ app/admin/regions.rb
@@ -1,19 +1,16 @@
 ActiveAdmin.register Region do
   menu false
   permit_params :name, :driver_id
-  # See permitted parameters documentation:
-  # https://github.com/activeadmin/activeadmin/blob/master/docs/2-resource-customization.md#setting-up-strong-parameters
-  #
-  # Uncomment all parameters which should be permitted for assignment
-  #
 
-  #
-  # or
-  #
-  # permit_params do
-  #   permitted = [:name, :driver_id]
-  #   permitted << :other if params[:action] == 'create' && current_user.admin?
-  #   permitted
-  # end
-  
+  show do
+    attributes_table do
+      row :name
+      row :driver_id
+      #row :driver_license do |driver|
+        #link_to(image_tag(url_for(driver.driver_license), style: 'width: 150px; height: 150px; object-fit: cover;'), url_for(driver.driver_license))
+      #end
+      column_chart Order.group(:timeslot_id).count()
+    end
+  end
+
 end
