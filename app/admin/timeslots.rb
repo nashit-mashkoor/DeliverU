ecd263e3ff50edffd9bf1b4834b5d3062e398153
Ahsan Naveed <ahsannaveed94@gmail.com>
2019-11-28 03:48:37 +0500

    added payables and fixed timeslot functionality

--- app/admin/timeslots.rb
+++ app/admin/timeslots.rb
@@ -1,5 +1,21 @@
 ActiveAdmin.register Timeslot do
   menu false
-  permit_params :start, :end, :region_id
+  permit_params :start, :region_id
 
+  show do
+    attributes_table do
+      row :start
+      row :region
+      row :created_at
+      row :updated_at
+    end
+  end
+
+  form do |f|
+    f.inputs "Please fill all fields" do
+      f.input :region, required: true
+      f.input :start, required: true
+    end
+    f.actions
+  end
 end
--- app/models/payable.rb
+++ app/models/payable.rb
@@ -1,4 +1,13 @@
 class Payable < ApplicationRecord
   enum type: [:to_pay, :to_recieve]
   belongs_to :driver
+  self.inheritance_column = nil
+
+  def color
+    self.type == 'to_recieve' ? 'blue' : 'red'
+  end
+  
+  def typeString
+    self.type == 'to_recieve' ? 'Recieval' : 'Payable'
+  end
 end
