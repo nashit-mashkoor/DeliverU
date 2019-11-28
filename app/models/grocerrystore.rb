d1ec89e68c7b90c998b0ac7c4cc7f13868640d23
Ahsan Naveed <ahsannaveed94@gmail.com>
2019-11-28 12:48:03 +0500

    added presence validation in grocery

--- app/models/grocerrystore.rb
+++ app/models/grocerrystore.rb
@@ -1,4 +1,6 @@
 class Grocerrystore < ApplicationRecord
     has_many :grocerryitems, dependent: :destroy
     has_many :orders, through: :grocerryitems
+
+    validates :name, :cost, presence: true
 end
