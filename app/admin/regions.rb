27e021de174044dd4587e076bc23726a57e631a2
Ahsan Naveed <ahsannaveed94@gmail.com>
2019-11-29 01:20:06 +0500

    added google maps for region selection

--- app/admin/regions.rb
+++ app/admin/regions.rb
@@ -1,11 +1,16 @@
 ActiveAdmin.register Region do
   menu false
-  permit_params :name, :TlLat, :TlLong, :BrLat, :BrLong
+  # actions :index, :show, :update, :edit, :new, :create
+  actions :all
 
   show do
     attributes_table do
       row :name
       row :driver
+      row :TlLat
+      row :TlLong
+      row :BrLat
+      row :BrLong
       # row :driver_license do |driver|
       #   link_to(image_tag(url_for(driver.driver_license), style: 'width: 150px; height: 150px; object-fit: cover;'), url_for(driver.driver_license))
       # end
@@ -14,5 +19,42 @@ ActiveAdmin.register Region do
       end
     end
   end
+
+  
+
+  form partial: 'region_map', locals: { resource: @resource }
+
+  controller do
+    def permitted_params
+      params.permit [:authenticity_token, :commit, :name, :TlLat, :TlLong, :BrLat, :BrLong, :id]
+    end
+
+    def create
+      region_params = [:name, :TlLat, :TlLong, :BrLat, :BrLong]
+
+      region_data = {}
+      region_params.each{|k| region_data[k] = permitted_params[k]}
+      
+
+      region = Region.new(region_data)
+      region.save!
+
+      redirect_to admin_regions_path()
+    end
+
+    def update
+      region_params = [:name, :TlLat, :TlLong, :BrLat, :BrLong]
+
+      region_data = {}
+      region_params.each{|k| region_data[k] = permitted_params[k]}
+      
+
+      region = Region.find(permitted_params[:id]).update_attributes(region_data)
+      # region.save!
+
+      redirect_to admin_region_path(resource)
+    end
+  end
+
 
 end
